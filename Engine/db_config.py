526377dc3ebc96b21adb6ac3d99d7a6b11b8c254
Stefan Popović <93131229+popovics00@users.noreply.github.com>
2023-01-23 00:36:39 +0100

    db update kupisajt

--- Engine/db_config.py
+++ Engine/db_config.py
@@ -4,17 +4,17 @@ from flask import Flask
 from flask_marshmallow import Marshmallow
 
 db = SQLAlchemy()
-DB_NAME = "drs-project-313635ceea"
+DB_NAME = "kupisajt"
 mysql = MySQL()
 ma = Marshmallow()
 
 app=Flask(__name__)
 app.config['SECRET_KEY'] = 'ioahdoah oaihdoah'
-app.config['MYSQL_DATABASE_USER'] = 'root-f35a'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Slatkamalasladakja123!'
+app.config['MYSQL_DATABASE_USER'] = 'kupisajt'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'eofZPPtWIQ29ugB'
 app.config['MYSQL_DATABASE_DB'] = DB_NAME
-app.config['MYSQL_DATABASE_HOST'] = 'mysql.gb.stackcp.com:49269'
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://root-f35a:Slatkamalasladakja123!@mysql.gb.stackcp.com:49269/{DB_NAME}'
+app.config['MYSQL_DATABASE_HOST'] = '149.102.155.173:3306'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://kupisajt:eofZPPtWIQ29ugB@149.102.155.173:3306/{DB_NAME}'
 
 mysql.init_app(app)
 db = SQLAlchemy(app)
